a947f04d6ded203e9aad6ab0db3b5a21c43d9fb0
jandresraba <jandresraba@gmail.com>
2020-10-15 18:44:55 -0500

    Se le agrega parte logica al metodo main

--- src/main/java/com/mycompany/miprimergit/Main.java
+++ src/main/java/com/mycompany/miprimergit/Main.java
@@ -15,7 +15,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Mis inicios en git");
+        System.out.println("Mis inicios en git");
+        System.out.println("Mis inicios en git");
+        System.out.println("Mis inicios en git");
+        for(int i =0; i<=10;i++){
+            System.out.println("Los numeros del 1 al 10 "+i);
+        }
     }
     
 }
